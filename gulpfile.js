dd516937e2f3b6c2637e3f0ac48098ab6c84318d
Huan Zhang <huan.zhang@trinitymirror.com>
2016-04-22 18:09:29 +0100

    added clean task

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,24 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var stylus = require('gulp-stylus');
+var del = require('del');
+
+//Clean
+
+gulp.task('clean', function() {
+    return del('dist');
+})
 
 //JS Task
-gulp.task('js', function() {
+gulp.task('js', ['clean'], function() {
     return gulp.src('src/js/app.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(gulp.dest('dist/js/'));
-})
+});
 
 //Compile Stylus
-gulp.task('stylus', function() {
+gulp.task('stylus', ['clean'], function() {
     return gulp.src('src/stylus/app.styl')
         .pipe(stylus())
         .pipe(gulp.dest('dist/css'));
